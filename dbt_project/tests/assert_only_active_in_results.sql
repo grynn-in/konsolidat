1c07ef202b271e1126e3821f03b92899a00db1b7
Deepak Pai <deepakpai@Deepaks-MacBook-Pro.local>
2026-06-21 12:03:37 +0200

    fix(test): rewrite assert_only_active_in_results as join for ClickHouse

--- dbt_project/tests/assert_only_active_in_results.sql
+++ dbt_project/tests/assert_only_active_in_results.sql
@@ -4,13 +4,11 @@ select
     aat.allocation_run_id,
     aat.run_status
 from {{ ref('gold_allocation_audit_trail') }} as aat
+inner join {{ ref('gold_allocation_results') }} as ar
+    on ar.fiscal_year = aat.fiscal_year
+    and ar.fiscal_period = aat.fiscal_period
+    and ar.allocation_rule_id = aat.allocation_rule_id
+    and ar.allocated_amount = aat.allocated_amount
 where aat.run_status = 'Reversed'
   and aat.allocated_amount > 0
-  and exists (
-    select 1 from {{ ref('gold_allocation_results') }} as ar
-    where ar.fiscal_year = aat.fiscal_year
-      and ar.fiscal_period = aat.fiscal_period
-      and ar.allocation_rule_id = aat.allocation_rule_id
-      and ar.allocated_amount = aat.allocated_amount
-  )
 limit 10
